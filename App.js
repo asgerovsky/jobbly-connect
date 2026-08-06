eb8d48066c9adf309306602a18799cce64533842
asgerovsky <teymur.esgerov.97@gmail.com>
2026-08-06 11:44:11 +0400

    Connect App with HomeScreen

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import HomeScreen from './src/screens/HomeScreen';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        Jobbly Connect
-      </Text>
-
-      <Text>
-        Xidmət bazarı tətbiqinə xoş gəlmisiniz
-      </Text>
+      <HomeScreen />
     </View>
   );
 }
@@ -18,12 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
   },
 });
