6eb1a0c2fcb2903182995af8fe7ff085bf6da29c
asgerovsky <teymur.esgerov.97@gmail.com>
2026-08-06 11:55:50 +0400

    Add role screen to navigation

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -5,7 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
-
+import RoleScreen from '../screens/RoleScreen';
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
