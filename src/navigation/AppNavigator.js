6ac220712749a63781ed1dfc63d97c53717efc7f
asgerovsky <teymur.esgerov.97@gmail.com>
2026-08-06 11:45:58 +0400

    Add app navigation

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'Jobbly Connect'
+          }}
+        />
+
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            title: 'Daxil ol'
+          }}
+        />
+
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{
+            title: 'Qeydiyyat'
+          }}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
